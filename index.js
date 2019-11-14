3cebeee7f73021354343c23500cf3b94f3ad0bca
h-nago <riomuisakuhinata@gmail.com>
2019-11-14 10:02:39 +0900

    ?

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const wait = require('./wait');
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
+    const token = core.getInput('repo-token', {required: true});
+    const octokit = new github.GitHub(token);
+    const pullRequests = await octokit.pulls.list({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    })
+    console.log(pullRequests);
+
+
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`)
 
@@ -13,15 +22,6 @@ async function run() {
     core.debug((new Date()).toTimeString())
 
     core.setOutput('time', new Date().toTimeString());
-
-
-    const token = core.getInput('repo-token', {required: true});
-    const octokit = new github.GitHub(token);
-    const pullRequests = await octokit.pulls.list({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo
-    })
-    console.log(pullRequests);
   } 
   catch (error) {
     core.setFailed(error.message);
